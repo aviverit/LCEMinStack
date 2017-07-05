044ec5aa1e53e3f99e89393831abf39cfff63716
aviverit <andrew.viverito@gmail.com>
2017-07-05 14:02:35 -0500

    v.0.1.0 "Starting"

---
+++ LCEMinStack/MinStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCEMinStack
+{
+    public class MinStack
+    {
+        int min;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+            min = 0;
+        }
+
+        public void Push(int x)
+        {
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public int Top()
+        {
+
+        }
+
+        public int GetMin()
+        {
+            return min;
+        }
+    }
+}
